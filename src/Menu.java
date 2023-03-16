1d7a2706a367bfb2e216e43d2d3e46238d951555
macbook <babayev.nicat@yandex.ru>
2023-03-16 18:55:11 +0400

    Обновленное меню

--- src/Menu.java
+++ src/Menu.java
@@ -6,21 +6,22 @@ public class Menu {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
-            System.out.println("Выберите действие введя его номер " +
-                    "1. Зашифровать текст в файле с помощью ключа" +
-                    "2. Расшифровать текст в файле с помощью ключа" +
-                    "3. Подобрать ключ к зашифрованному тексту в файле " +
-                    "4. Расшифровать текст в файле методом статического анализа " +
-                    "5. Выход из программы");
+            System.out.println("""
+                    Выберите действие введя его номер\s
+                    1. Зашифровать текст в файле с помощью ключа\s
+                    2. Расшифровать текст в файле с помощью ключа\s
+                    3. Подобрать ключ к зашифрованному тексту в файле\s
+                    4. Расшифровать текст в файле методом статического анализа\s
+                    5. Выход из программы""");
 
             String answer = reader.readLine();
 
             switch (answer) {
-                case "1" -> System.out.println("1. Зашифровать текст в файле с помощью ключа");
-                case "2" -> System.out.println("2. Расшифровать текст в файле с помощью ключа");
+                case "1" -> new Encrypted().encrypted();
+                case "2" -> new Decrypted().decrypted();
                 case "3" -> System.out.println("3. Подобрать ключ к зашифрованному тексту в файле ");
                 case "4" -> System.out.println("4. Расшифровать текст в файле методом статического анализа ");
-                default -> {
+                case "5" -> {
                     return;
                 }
 
