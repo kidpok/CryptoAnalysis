8f8bfee1710b179371ee6fdc0dcd341e1fbf40e8
macbook <babayev.nicat@yandex.ru>
2023-03-18 16:45:21 +0400

    Добавили новый класс

---
+++ src/PathHelper.java
@@ -0,0 +1,24 @@
+import java.nio.file.Path;
+
+
+public class PathHelper {
+
+    private PathHelper() {
+    }
+
+    public static Path buildFileName(String path, String suffix) {
+
+        Path fullPath = Path.of(path);
+        Path directory = fullPath.getParent();
+        String fileName = fullPath.getFileName().toString();
+        String newFileName;
+        if (fileName.contains(".")) {
+            int index = fileName.lastIndexOf(".");
+            newFileName = fileName.substring(0, index) + suffix + fileName.substring(index);
+        } else {
+            newFileName = fileName + suffix;
+        }
+
+        return directory.resolve(Path.of(newFileName));
+    }
+}
